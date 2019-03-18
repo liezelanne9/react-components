e163aa0bf54a0905513a925068ca590d2ff29a52
Liezel Manalo <liezelanne9@gmail.com>
2019-03-18 12:09:29 -0700

    forgot to commit when completed 2 weeks prior

--- app.js
+++ app.js
@@ -1,1 +1,81 @@
 // TODO
+
+class GroceryListItem extends React.Component {
+    
+    constructor(props) {
+         // Equivalent to ES5's React.Component.call(this, props)
+        super(props);
+
+        // `state` is just an object literal
+        this.state = {
+            done: false
+        };
+    }
+
+    // When a list item is clicked, we will toggle the `done`
+    // boolean, and our component's `render` method will run again
+    onListItemClick() {
+        this.setState({
+            done: !this.state.done
+        });
+        if (this.state.done === false) {
+            alert('NICE');
+        }
+    }
+
+    // Every class component must have a `render` method
+    // Stateless functional components are pretty much just this method
+    render() {
+        var style = {
+            textDecoration: this.state.done ? 'line-through' : 'none'
+        };
+
+    // `props` is no longer passed as an argument,
+    // but instead accessed with `this.props`
+    return (
+      <li style= {style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>,
+      (<Fruits />)
+    );
+
+  }
+}
+
+// a component
+var Dairy = () => (
+    <ul>
+        <li>Eggs</li>
+        <li>Almond Milk</li>
+    </ul>
+    
+)
+
+// a component
+var Fruits = () => (
+    <ul>
+        <li>Bananas</li>
+        <li>Oranges</li>
+    </ul>
+)
+
+// a component using the class component GroceryListItem
+var GroceryList = (props) => {
+
+    return (
+        <ul>
+            {props.groceryItems.map(item =>
+                <GroceryListItem item={item} />)}
+        </ul>
+    );
+};
+
+var GroceryListApp = () => (
+
+    <ul>
+        <Dairy />
+        <GroceryList groceryItems={['Cereal', 'Coffee', 'Bread']}/>
+    </ul>
+
+)
+
+ReactDOM.render(<GroceryListApp />, document.getElementById("app"));
+ReactDOM.render(<Fruits />, document.getElementById("other"));
